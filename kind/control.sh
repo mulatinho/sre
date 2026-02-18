0a7046cfcbe44f9ee7bb71466a17640e127590e0
Alexandre Mulatinho <alex@mulatinho.net>
2026-02-18 11:33:57 -0300

    control start and stop of the kind clusters
    Signed-off-by: Alexandre Mulatinho <alex@mulatinho.net>

---
+++ kind/control.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+		
+help() {
+	echo "$0 <start|stop>";
+	exit 1
+}
+
+if [ ! "$1 " ] ; then
+	help
+fi
+
+case "$1" in
+	"start")
+		docker start $(docker ps -aq --filter "name=kind-")
+		;;
+	"stop")
+		docker stop $(docker ps -q --filter "name=kind-")
+		;;
+	*)
+		help
+		;;
+esac
